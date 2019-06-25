9b7b77249cf09bab0376d37dbe16fe48eedb6f8e
Hüseyin Bilginer <bilginerhuseyinofficial@gmail.com>
2019-06-25 23:40:23 +0300

    02_Console_ReadLineAndReadKey

--- 01_Console_WriteAndWriteLine/01_Console_WriteAndWriteLine/Program.cs
+++ 01_Console_WriteAndWriteLine/01_Console_WriteAndWriteLine/Program.cs
@@ -19,6 +19,8 @@ namespace _01_Console_WriteAndWriteLine
 
 
             Console.WriteLine("Murat Bilginer");
+
+
         }
     }
 }
---
+++ 02_Console_ReadLineAndReadKey/02_Console_ReadLineAndReadKey/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _02_Console_ReadLineAndReadKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             
+             Console.ReadLine() Metodu klavyeden değer okumak için kullanılan metoddur.
+             
+             
+             */
+
+            Console.Write("Adınızı Giriniz: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Soyadınızı Giriniz: ");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Adınız ve Soyadınız: "+Name+" "+LastName);
+
+
+            Console.WriteLine();
+
+
+            Console.Write("Doğum tarihini giriniz: ");
+            string BT = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Doğum yeriniz: ");
+            string DY = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("DY ve BT: " + DY + " " + BT);
+
+
+
+
+        }
+    }
+}

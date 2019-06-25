1cfb848b4485d5e87f7d4be7302c0e5e8b54e82d
Hüseyin Bilginer <bilginerhuseyinofficial@gmail.com>
2019-06-25 20:16:49 +0300

    01_Console_WriteAndWriteLine

---
+++ 01_Console_WriteAndWriteLine/01_Console_WriteAndWriteLine/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01_Console_WriteAndWriteLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write ile tek satıra yazı yazabiliriz.
+            // WriteLine ile bir alt satıra geçecek şekilde yazı yazabiliriz.
+
+            Console.Write("Hüseyin Bilginer");
+            Console.WriteLine();
+            Console.Write("Hello World");
+            Console.WriteLine();
+            Console.Write("Hello World 2");
+            Console.WriteLine();
+            Console.WriteLine("Murat Bilginer");
+
+
+            Console.WriteLine("Murat Bilginer");
+        }
+    }
+}
